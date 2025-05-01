436c19e1de5b627c6d91628208e9b42059fe274a
Svyatoslav Drozdov <sddrozdov000@gmail.com>
2025-05-01 16:53:09 +0300

    add OutlinedTextField login and password

--- app/src/main/java/com/sddrozdov/newsappcompose/ui/screen/LoginScreen.kt
+++ app/src/main/java/com/sddrozdov/newsappcompose/ui/screen/LoginScreen.kt
@@ -1,17 +1,23 @@
 package com.sddrozdov.newsappcompose.ui.screen
 
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.ModalDrawerSheet
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.outlined.Email
+import androidx.compose.material.icons.outlined.Lock
+import androidx.compose.material3.Icon
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.vector.rememberVectorPainter
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -33,7 +39,29 @@ fun LoginScreen(
         )
         Image(
             painter = painterResource(id = R.drawable.log_image_news),
-            contentDescription = "Login image news"
+            contentDescription = "Login image news",
+            modifier = Modifier.padding(top = 15.dp)
+        )
+        OutlinedTextField(
+            value = "Email",
+            onValueChange = {},
+            leadingIcon = {
+                Icon(
+                    painter = rememberVectorPainter(image = Icons.Outlined.Email),
+                    contentDescription = ""
+                )
+            }, modifier = Modifier.padding(top = 15.dp)
+        )
+        OutlinedTextField(
+            value = "Password",
+            onValueChange = {},
+            leadingIcon = {
+                Icon(
+                    painter = rememberVectorPainter(image = Icons.Outlined.Lock),
+                    contentDescription = ""
+                )
+            }, modifier = Modifier.padding(top = 15.dp),
+            visualTransformation = PasswordVisualTransformation()
         )
     }
 }
