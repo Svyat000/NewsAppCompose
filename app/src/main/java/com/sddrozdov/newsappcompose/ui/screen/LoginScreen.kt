34b15308e1920299825a5f5a1e2a6d0668d296fd
Svyatoslav Drozdov <sddrozdov000@gmail.com>
2025-05-01 16:16:46 +0300

    add image and strings

--- app/src/main/java/com/sddrozdov/newsappcompose/ui/screen/LoginScreen.kt
+++ app/src/main/java/com/sddrozdov/newsappcompose/ui/screen/LoginScreen.kt
@@ -1,20 +1,45 @@
 package com.sddrozdov.newsappcompose.ui.screen
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ModalDrawerSheet
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.sddrozdov.newsappcompose.R
 import com.sddrozdov.newsappcompose.ui.navigation.Screen
 
 @Composable
 fun LoginScreen(
-    onNavigateTo:(Screen) -> Unit
-){
-    Column(verticalArrangement = Arrangement.Center,
+    onNavigateTo: (Screen) -> Unit = {}
+) {
+    Column(
+        modifier = Modifier.fillMaxSize(),
         horizontalAlignment = Alignment.CenterHorizontally
-
-    ){
-        Text(text = "LoginScreen")
+    ) {
+        Text(
+            text = stringResource(id = R.string.app_name),
+            fontSize = 32.sp,
+            modifier = Modifier.padding(top = 110.dp)
+        )
+        Image(
+            painter = painterResource(id = R.drawable.log_image_news),
+            contentDescription = "Login image news"
+        )
     }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun LoginScreenPreview() {
+    LoginScreen()
 }
