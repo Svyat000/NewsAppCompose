7f36f9a09f270ae15965b0f5cf79aadb8b65de7c
Svyatoslav Drozdov <sddrozdov000@gmail.com>
2025-05-05 17:31:40 +0300

    add sealed class LoginScreenEvent

--- app/src/main/java/com/sddrozdov/newsappcompose/ui/screen/state/LoginScreenState.kt
+++ app/src/main/java/com/sddrozdov/newsappcompose/ui/screen/state/LoginScreenState.kt
@@ -4,3 +4,8 @@ data class LoginScreenState(
     val email: String = "",
     val password: String = "",
 )
+
+sealed class LoginScreenEvent {
+    data class EmailUpdated(val newEmail: String) : LoginScreenEvent()
+    data class PasswordUpdated(val newPassword: String) : LoginScreenEvent()
+}
