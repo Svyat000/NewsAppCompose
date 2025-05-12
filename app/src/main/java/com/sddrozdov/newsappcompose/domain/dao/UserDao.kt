6ec6e5f047a60ba67a621a089588dec34d7cde7f
Svyatoslav Drozdov <sddrozdov000@gmail.com>
2025-05-12 17:38:16 +0300

    add UserDao

---
+++ app/src/main/java/com/sddrozdov/newsappcompose/domain/dao/UserDao.kt
@@ -0,0 +1,21 @@
+package com.sddrozdov.newsappcompose.domain.dao
+
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.Query
+import androidx.room.Update
+import com.sddrozdov.newsappcompose.domain.entity.User
+
+@Dao
+interface UserDao {
+    @Insert
+    fun addUser(user: User)
+
+    @Delete
+    fun deleteUser(user: User)
+
+    @Query("SELECT * FROM user WHERE email=:email")
+    fun getUserByEmail(email: String): User?
+
+}
